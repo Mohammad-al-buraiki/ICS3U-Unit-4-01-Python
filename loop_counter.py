ff2187bbe95fbed9b5181568f850df21bb01830b
Ubuntu <ubuntu@ip-172-31-5-27.ec2.internal>
2021-01-03 18:56:27 +0000

    loop_counter.py

---
+++ loop_counter.py
@@ -0,0 +1,28 @@
+# !/usr/bin/env python3
+
+# Created by: Mohammad-al-buraiki
+# Created on December 2020
+# This program is a loop a while loop
+
+
+def main():
+    # this function uses a while loop
+    loop_counter = 0
+
+    # input
+    print("")
+    positive_integer = input("Enter how many times to repeat: ")
+
+    # process and output
+    try:
+        positive_integer = int(positive_integer)
+        while loop_counter < positive_integer:
+            print("{0} time through loop.", sum(loop_counter))
+            loop_counter = loop_counter + 1
+
+    except Exception:
+        print("sorry, this was not an integer")
+
+
+if __name__ == "__main__":
+    main()
